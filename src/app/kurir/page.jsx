bfd14823081275caa946beb9323fffe6de39f662
anandaalex <11221032@student.itk.ac.id>
2025-05-02 21:47:32 +0800

    menambahkan dashboard kurir

---
+++ src/app/kurir/page.jsx
@@ -0,0 +1,11 @@
+import DashboardLayout from "../components/DashboardLayout";
+
+const KurirDashboard = () => {
+  return (
+    <DashboardLayout role="courier" username="Ahmad">
+      <p>Konten statistik admin atau laporan bisa ditaruh di sini nanti.</p>
+    </DashboardLayout>
+  );
+};
+
+export default KurirDashboard;
