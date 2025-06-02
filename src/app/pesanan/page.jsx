aa2136c97bd50c33ba276c0c88a899d697c0cc18
febrynn <11221008@student.itk.ac.id>
2025-06-02 17:54:53 +0800

    nyoba pesanan

--- src/app/pesanan/page.jsx
+++ src/app/pesanan/page.jsx
@@ -2,19 +2,16 @@
 
 import PesananCard from "../components/PesananCard";
 import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
 
-const pesanan = () => {
+const Pesanan = () => {
   return (
     <>
       <Navbar />
-      <main className="max-w-7xl pt-[200px] p-6 space-y-4 mx-auto">
-        <h2 className="font-medium text-lg">Detail Pesanan Anda</h2>
+      <main className="pt-[200px] p-6 space-y-4">
         <PesananCard />
       </main>
-      <Footer />
     </>
   );
 };
 
-export default pesanan;
+export default Pesanan;
